15eed440ace1cb515969a1a99cbd0094d5e41eda
akshaysb100 <akshaybavalekar100@gmail.com>
2019-11-21 09:47:38 +0530

    Add:create pattern for first name validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
 echo "Welcome User Registration page"
+
+echo "Enter user first name"
+read name
+namePattern="^[A-Z]([A-Za-z]{2,})$"
+
+if [[ $name =~ $namePattern ]]
+then
+	echo "valid user name"
+else
+	echo "First name starts with Cap and has minimum 3 characters"
+fi
+
